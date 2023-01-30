9d60f072dcbdc6c839419cda6065891e04733af6
Diego Jimenez <diegojimenez@Diegos-MacBook-Air.local>
2023-01-29 21:30:40 -0500

    modeling a bank account using classses.

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-## THIS IS WHERE I WILL BE WRITTING MOST OF MY PROGRAMS 
+ 
+
+
+# Creating classes
+class BankAccount(object):
+    def __init__(self, name):
+        self.name = name 
+        self.balance = 0
+
+    def deposit(self, amount):
+        if amount > 0:
+            new_balance = self.balance + amount
+            self.balance = new_balance
+        
+    def withdraw(self, amount):
+        if amount > 0:
+            new_balance = self.balance - amount
+            self.balance = new_balance
+    
+    def __str__(self) -> str:
+        return 'Account "' + self.name + '" has a balance of ' + str(self.balance)
+
