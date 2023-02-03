c11bd2e1df836aa8e0cd06a86a4474900e2d25f5
Diego Jimenez <diegojimenez@Diegos-MacBook-Air.local>
2023-02-03 17:46:35 -0500

    working with loops

--- main.py
+++ main.py
@@ -31,3 +31,52 @@ print(b1.name)
 # acct.withdraw(50)
 # print(acct) # Account "MyAccount" has a balance of 250.
 
+
+
+
+# nums = [0, 1, 2, 3, 4, 5, 6]
+# nums = ['1', '2', '3', '4', '5', '6']
+
+# nums[0] =6
+# seq = 
+# for index in range(len(nums)):
+#     # value  = nums[index]
+#     # print(repr(value), type(value))
+#     print(nums[index])
+# add_one = set([1, 3, 5])
+# mul_by_two = set([2, 4])
+# div_by_two = set([4,6])
+# output_list = []
+
+# for num in nums:
+#     if num in add_one:
+#         output_list.append( num + 1)
+#     elif num in mul_by_two:
+#         output_list.append(num * 2)
+#     elif num in div_by_two:
+#         output_list.append(num / 2)
+  
+
+# indexies = iter(range(len(nums)))
+# print(next(indexies))
+# print(next(indexies))
+# print(next(indexies))
+# print(next(indexies))
+# print(next(indexies))
+# print(next(indexies))
+# print(next(indexies))
+# nums.
+
+# at_num = iter(nums)
+# print(next(at_num))
+# print(next(at_num))
+# print(next(at_num))
+
+nums = list(x for x in range(1, 6))
+# nums = []
+
+name = "Dario"
+for num in nums:
+    last_name ="Arias"
+
+print(name, last_name)
